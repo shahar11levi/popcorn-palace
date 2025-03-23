9c91accf431536e64b348f9c5a726b309261cde7
shahar.levi <shahar11levi@gmail.com>
2025-03-23 19:05:55 +0200

    Handle adding showtime when theater occupied

--- src/main/java/com/att/tdp/popcorn_palace/service/ShowtimeService.java
+++ src/main/java/com/att/tdp/popcorn_palace/service/ShowtimeService.java
@@ -6,6 +6,8 @@ import com.att.tdp.popcorn_palace.model.Movie;
 import com.att.tdp.popcorn_palace.model.Showtime;
 import com.att.tdp.popcorn_palace.repository.ShowtimeRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ShowtimeService {
 
@@ -25,10 +27,15 @@ public class ShowtimeService {
         return showtime;
     }
 
+    @Transactional
     public Showtime addShowtime(Showtime showtime) {
         Movie movie = showtime.getMovie();
         if (movie == null || movieService.findByTitle(movie.getTitle()) == null)
             throw new RuntimeException("The movie " + movie.getTitle() + " not found");
+        if (showtime.getStartTime().after(showtime.getEndTime()))
+            throw new RuntimeException("The start time must be before the end time");
+        if (isTheaterOccupied(showtime))
+            throw new RuntimeException("The theater is already occupied");
         return showtimeRepository.save(showtime);
     }
 
@@ -52,6 +59,16 @@ public class ShowtimeService {
         } else {
             throw new RuntimeException("The showtime " + id + " not found");
         }
+    }
+
+    private boolean isTheaterOccupied(Showtime showtime) {
+        for (Showtime s : showtimeRepository.findAll()) {
+            if (s.getTheater().equals(showtime.getTheater())) {
+                if (! (s.getStartTime().after(showtime.getEndTime()) || s.getEndTime().before(showtime.getStartTime())))
+                    return true;
+            }
+        }
+        return false;
     }
 
 
